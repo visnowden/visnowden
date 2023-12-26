c7f635634b410fdc345d4475a18b79b34138c3ee
Code_H29/ <72585285+visnowden@users.noreply.github.com>
2023-12-26 00:16:44 -0300

    Delete .github/workflows directory

--- .github/workflows/streak/compare_update.py
+++
@@ -1,6 +0,0 @@
-# init
-from requests import get
-
-svg_string = get('https://streak-stats.demolab.com?user=visnowden').text
-streak_number = int(svg_string[svg_string.find('<!-- Current Streak big number -->'):].split('\n')[3].strip())
-print(streak_number)
