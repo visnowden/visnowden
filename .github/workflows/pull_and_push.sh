94f07fba41c6e38a028f0425b18812e962b5c97a
Code_H29/ <72585285+visnowden@users.noreply.github.com>
2023-12-25 21:23:59 -0300

    Delete .github/workflows/pull_and_push.sh

--- .github/workflows/pull_and_push.sh
+++
@@ -1,5 +0,0 @@
-git checkout -b output
-git config --global user.name "visnowden"
-git config --global user.email "72585285+visnowden@users.noreply.github.com"
-git add .
-git push -u origin output -f
