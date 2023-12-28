818dc8b8781c8964c41d68488460ac8005eb504a
Code_H29/ <72585285+visnowden@users.noreply.github.com>
2023-12-28 18:38:25 -0300

    Update 👀.py

--- .github/workflows/👀.py
+++ .github/workflows/👀.py
@@ -1,5 +1,5 @@
 from datetime import datetime;from requests import get;from time import sleep,time;streak_number=i=''
-while datetime.now().strftime("%H:%M:%S")[:5]!='13:00':
+while int(datetime.now().strftime("%H:%M:%S")[:3])>12:
  start=time()
  try:
   svg_string=get('https://streak-stats.demolab.com?user=visnowden').text;streak_number=int(svg_string[svg_string.find('<!-- Current Streak big number -->'):].split('\n')[3].strip())
