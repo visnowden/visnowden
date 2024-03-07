310a360f5c2fd1654af64928524be928ec616038
Code_H29/ <72585285+visnowden@users.noreply.github.com>
2024-03-07 05:58:45 -0300

    Update compare_update.py

--- .github/workflows/streak/compare_update.py
+++ .github/workflows/streak/compare_update.py
@@ -4,7 +4,6 @@ from requests import get
 
 print(f'Files and directories: {listdir()}')
 print(f'Current directory: {getcwd()}')
-print(getcwd())
 
 svg_string = get('https://streak-stats.demolab.com?user=visnowden').text
 streak_number = int(svg_string[svg_string.find('<!-- Current Streak big number -->'):].split('\n')[3].strip())
